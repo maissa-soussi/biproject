6186d2cdc94141eaf66ddc400d155b475222ba9a
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2022-01-06 22:36:04 +0100

    topdestination api

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +10,28 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
   destinationVar=false
   airlineVar=false
-  public destinations: any = {}
-  public airlines: any = {}
-  constructor(private router: Router) { }
+  public destinations: any 
+  public airlines: any 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>("http://localhost:5000/destination")
+        .subscribe(
+          (result) => {
+            this.destinations = result;
+            console.log(this.destinations);           
+          },
+          (error) => {console.log(error); }
+        );
+    
+    this.http.get<any>("http://localhost:5000/airline")
+        .subscribe(
+          (result) => {
+            this.airlines = result;
+            console.log(this.airlines);           
+          },
+          (error) => {console.log(error); }
+        );
   }
 
   destination(){

2133ad76526136051283715acba2f85209c11446
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2022-01-06 21:42:12 +0100

    recommmandation api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,7 +14,8 @@ import { RecommendationComponent } from './recommendation/recommendation.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-recommendation',
@@ -8,12 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class RecommendationComponent implements OnInit {
   public countries: any = []
   public cities: any = []
-  constructor() { }
+  user: any
+  recommandation: any = []
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>("http://localhost:5000/country")
+        .subscribe(
+          (result) => {
+            this.countries = result;
+            console.log(this.countries);           
+          },
+          (error) => {console.log(error); }
+        )
   }
 
-  recommendation(){
+  getCity(country:any): void {
+    this.http.get<any>("http://localhost:5000/city/" + country)
+        .subscribe(
+          (result) => {
+            this.cities = result;          
+          },
+          (error) => {console.log(error); }
+        )
+  }
+  recommendation(): void{
+    this.http.post("http://127.0.0.1:5000/recomm",this.user).subscribe(
+      (result) => {
+        this.recommandation = result;          
+      },
+      (error) => {console.log(error); }
+    );
     
     
   }
